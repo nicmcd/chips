532d44f6a27f4a2e35d2bf016d9de14843ca169d
Nic McDonald <n.mcdonald83@gmail.com>
2016-03-21 14:50:23 -0700

    two column legend and better viewing

--- chipplot.py
+++ chipplot.py
@@ -22,7 +22,7 @@ def plot_bandwidth(db, plotfile, view=False):
   markers = [s[0] for s in styles]#[0:len(years)]
   colors = [s[1] for s in styles]#[0:len(years)]
 
-  fig = plt.figure(figsize=(10, 6))
+  fig = plt.figure(figsize=(16, 12))
   ax1 = fig.add_subplot(1, 1, 1)
 
   for idx in range(len(m['label'])):
@@ -43,8 +43,8 @@ def plot_bandwidth(db, plotfile, view=False):
       m = re.search('mathdefault\{10\^\{(.*)\}\}', ytext)
       exp = int(m.group(1))
       assert exp >= 0
-      for unit in ['bps', 'kbps', 'Mbps', 'Gbps', 'Tbps', 'Pbps', 'Ebps', 'Zbps',
-                   'Ybps']:
+      for unit in ['bps', 'kbps', 'Mbps', 'Gbps', 'Tbps', 'Pbps', 'Ebps',
+                   'Zbps', 'Ybps']:
         if exp < 3:
           ytext = '{0} {1}'.format(10**exp, unit)
           break
@@ -63,14 +63,17 @@ def plot_bandwidth(db, plotfile, view=False):
   ax1.yaxis.grid(b=True, which='major', color='k', linestyle='-')
 
   # shrink current x-axis
-  shrink = 0.30
+  shrink = 0.40
   box = ax1.get_position()
   ax1.set_position([box.x0, box.y0, box.width * (1.0-shrink), box.height])
 
   # add legend to the right of plot
-  ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5), scatterpoints=1)
+  ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5), scatterpoints=1,
+             fontsize=12, ncol=2)
 
-  #fig.tight_layout()
+  # axis labels and title
+  ax1.set_xlabel('Year')
+  ax1.set_ylabel('I/O Bandwidth')
 
   # show in GUI
   if view:
